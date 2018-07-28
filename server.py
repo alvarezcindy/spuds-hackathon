427d837aea04667b0e48914a3e6135c9f1bb323a
Cindy Alvarez <calvarez519@gmail.com>
2018-07-28 23:49:38 +0000

    Added sms route

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, redirect, flash
 from twilio.rest import Client
+from twilio import twiml
 import json
 from pprint import pprint
 
@@ -15,22 +16,31 @@ auth_token = 'b6cf30d56a1d725f6974f2838ec9606e'
 
 client = Client(account_sid, auth_token)
 
-@app.route("/sms")
-def sms_reply():
+@app.route("/sms", methods=['POST'])
+def sms():
 # twilio receives incoming text message
+    number = request.form['From']
+    body = request.form['Body']
 
-message = client.messages.create(
-                              body='TESTING HACKATHON!!',
-                              from_='+18646591878',
-                              to='+19253007799'
-                          )
+    response = 'We got this!'
+
+    resp = twiml.Response()
+    resp.message(f'Hello! {number}, your ailment is {response}')
+
+    return str(resp)
+
+def send_sms():
+
+    message = client.messages.create(
+                                  body='TESTING HACKATHON!!',
+                                  from_='+18646591878',
+                                  to='+19253007799'
+                              )
 
 @app.route('/', methods=[POST])
 def geolocate_user():
     """Find the user with cell towers and their phone #"""
     https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyD_DOgSSqBrqTUFqeqeudDT6XlOtZ8huZQ
-    
-print(message.sid)
 
 if __name__ == "__main__":
     app.run(debug=True)
